bcf2278a04879fbc5f904ffe32711815dbaf27ae
Tomasz Błahut <rzuf22@gmail.com>
2016-12-03 22:39:43 +0100

    Cosmetic changes

--- BeaconAGH/Properties/AssemblyInfo.cs
+++ BeaconAGH/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BeaconAGH.BL")]
+[assembly: AssemblyTitle("BeaconAGH")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BeaconAGH.BL")]
+[assembly: AssemblyProduct("BeaconAGH")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
